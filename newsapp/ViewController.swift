5473c058acc9242ec93aef04f5c1846f9ff79a5f
Jishnu Biju <jishnu.biju@litmus7.com>
2025-03-04 16:05:08 +0530

    Initial Commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by Jishnu Biju [Litmus7] on 04/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ newsappTests/newsappTests.swift
@@ -0,0 +1,17 @@
+//
+//  newsappTests.swift
+//  newsappTests
+//
+//  Created by Jishnu Biju [Litmus7] on 04/03/25.
+//
+
+import Testing
+@testable import newsapp
+
+struct newsappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
