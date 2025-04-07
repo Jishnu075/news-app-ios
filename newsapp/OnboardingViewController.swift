8d5795ea25b471fce0f04a5146db6278e43b45b9
Jishnu Biju <jishnubiju075@gmail.com>
2025-04-07 16:28:49 +0530

    test

--- newsapp/OnboardingViewController.swift
+++ newsapp/OnboardingViewController.swift
@@ -7,37 +7,41 @@
 
 import UIKit
 
-class OnboardingViewController: UIPageViewController, UIPageViewControllerDataSource{
+class OnboardingViewController: UIViewController
+//: UIPageViewController, UIPageViewControllerDataSource
+{
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
     //    let pages = ["onboardingOne, onboardingTwo"]
     //
     
-    let vcArray: [UIViewController] = [
-        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "onboardingOne"),
-        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "onboardingTwo"),
-    ]
+//    let vcArray: [UIViewController] = [
+//        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "onboardingOne"),
+//        UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "onboardingTwo"),
+//    ]
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.dataSource = self
-        if let firstVC = vcArray.first {
-            setViewControllers([firstVC], direction: .forward, animated: true)
-        }
+//        self.dataSource = self
+//        if let firstVC = vcArray.first {
+//            setViewControllers([firstVC], direction: .forward, animated: true)
+//        }
     }
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let index =  vcArray.lastIndex(of: viewController) else { return nil }
-        print(index)
-        let previousIndex = index - 1
-        guard previousIndex >= 0 else { return nil }
-        guard vcArray.count > previousIndex else { return nil }
-        return vcArray[previousIndex]
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let index = vcArray.lastIndex(of: viewController) else { return nil }
-        print(index)
-        let nextIndex = index + 1
-        guard nextIndex < vcArray.count else { return nil }
-        return vcArray[nextIndex]
-    }
+//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+//        guard let index =  vcArray.lastIndex(of: viewController) else { return nil }
+//        print(index)
+//        let previousIndex = index - 1
+//        guard previousIndex >= 0 else { return nil }
+//        guard vcArray.count > previousIndex else { return nil }
+//        return vcArray[previousIndex]
+//    }
+//    
+//    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+//        guard let index = vcArray.lastIndex(of: viewController) else { return nil }
+//        print(index)
+//        let nextIndex = index + 1
+//        guard nextIndex < vcArray.count else { return nil }
+//        return vcArray[nextIndex]
+//    }
 }
